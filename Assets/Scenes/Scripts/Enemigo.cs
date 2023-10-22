b792160326218688b0c2bc6164fbe94d3f173a59
CaRoxas <carlotacarballolol@gmail.com>
2023-10-22 19:34:40 +0100

    Día 22/10
    Intentar que se mueva el enemigo, INTENTAR

--- Assets/Scenes/Scripts/Enemigo.cs
+++ Assets/Scenes/Scripts/Enemigo.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class Enemigo : MonoBehaviour
 {
     public GameObject renacuajo;
+    private Transform objetivo;
+    public float velocidad = 2f;
     // Start is called before the first frame update
     void Start()
     {
         renacuajo.transform.LookAt(transform.position);
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var step = velocidad * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, objetivo.position, step);
     }
 }
