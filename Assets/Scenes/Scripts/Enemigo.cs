cce998be3921df76116244c8957274d605435c3a
CaRoxas <carlotacarballolol@gmail.com>
2023-10-24 23:26:51 +0100

    Dia 24/10 Update
    Añadir las imágenes bien escaladas y configurarlas para que salgan en su momento, mejorar la colocación de las monedas y hacer que aparezcan los segundos finales

--- Assets/Scenes/Scripts/Enemigo.cs
+++ Assets/Scenes/Scripts/Enemigo.cs
@@ -7,6 +7,7 @@ public class Enemigo : MonoBehaviour
     public Transform objetivo;
     public float velocidad = 50f;
     public GameObject GameOver;
+    public AudioSource Principal;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +27,7 @@ public class Enemigo : MonoBehaviour
         {
             PauseGame();
             GameOver.SetActive(true);
+            Principal.Stop();
         }
     }
     void PauseGame()
--- Assets/Scenes/Scripts/Jugador.cs
+++ Assets/Scenes/Scripts/Jugador.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class Jugador : MonoBehaviour
 {
     Rigidbody rb;
+    public TMP_Text Transcurso;
     float movZ, movX;
     public float speed = 10f;
     bool quiereSaltar = false;
     bool estaSuelo = true;
     int monedas = 9;
     float tiempo = 0;
+    public GameObject GameOver;
+    public GameObject Final;
+    public AudioSource Principal;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Debug.Log("Recomendado recoger primero la ficha del enemigo y no perder el tiempo traspasando grandes montañas :D");
     }
 
     // Update is called once per frame
@@ -39,15 +46,22 @@ public class Jugador : MonoBehaviour
         {
             estaSuelo = true;
         }
+        if (col.gameObject.tag == "Muerte")
+        {
+            PauseGame();
+            GameOver.SetActive(true);
+            Principal.Stop();
+        }
         if (col.gameObject.tag == "Monedas")
         {
             monedas = monedas - 1;
         }
         else if (monedas == 0)
-        { 
+        {
+            Final.SetActive(true);
+            Transcurso.text = "GG :D Total " + tiempo.ToString();
             Debug.Log(tiempo);
             PauseGame();
-           
         }
     }
     void Movimiento()
