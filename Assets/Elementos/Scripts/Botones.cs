e9d75ff6aeff6c08db82603d700518d2e47176e7
CaRoxas <carlotacarballolol@gmail.com>
2023-11-09 16:09:25 +0000

    Dia 09/11
    Arreglar los botones derecha izquierda para que cambien de idioma, añadir luz a las monedas (sin usarlas)

---
+++ Assets/Elementos/Scripts/Botones.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+
+public class Botones : MonoBehaviour
+{
+    public GameObject boton;
+    public static int indice = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void MoverDerecha()
+    {
+        indice++;
+        Debug.Log("Idioma pos :" + indice);
+        if (indice > 3)
+        {
+            indice = 0;
+        }
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[indice];
+    }
+    public void MoverIzquierda()
+    {
+        indice--;
+        Debug.Log("Idioma pos :" + indice);
+        if (indice < 0)
+        {
+            indice = 3;
+        }
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[indice];
+    }
+}
